0ffece43db2847256891f77ffe28165c955b97df
Matt Burroughs <matt.burro@gmail.com>
2024-03-22 15:31:05 -0400

    Create DbContext and add connection string to appsettings

---
+++ USTrails/USTrails.API/Data/USTrailsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using USTrails.API.Models.Domain;
+
+namespace USTrails.API.Data
+{
+    public class USTrailsDbContext : DbContext
+    {
+        public USTrailsDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Trail> Trails { get; set; }
+    }
+}
