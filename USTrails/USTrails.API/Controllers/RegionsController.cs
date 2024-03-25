6bd17f1ad47daf85b05e45d1ea46bbc2904a7eef
Matt Burroughs <matt.burro@gmail.com>
2024-03-25 17:50:57 -0400

    Create region repository and controller

---
+++ USTrails/USTrails.API/Controllers/RegionsController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using USTrails.API.Models.Domain;
+using USTrails.API.Models.DTO;
+using USTrails.API.Repositories;
+
+namespace USTrails.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // Get regions from database
+            var regions = await regionRepository.GetAllAsync();
+
+            // Return DTOs
+            return Ok(mapper.Map<List<RegionDto>>(regions));
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            // Get matching region from database
+            var region = await regionRepository.GetByIdAsync(id);
+
+            // Check if region is null
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            // Return DTO
+            return Ok(mapper.Map<RegionDto>(region));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddRegionRequestDto requestDto)
+        {
+            // Map DTO to domain model
+            var region = mapper.Map<Region>(requestDto);
+
+            // Use domain model to create region
+            region = await regionRepository.CreateAsync(region);
+
+            // Return DTO
+            return CreatedAtAction(nameof(GetById), new { id = region.Id }, mapper.Map<RegionDto>(region));
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto requestDto)
+        {
+            // Map DTO to domain model
+            var region = mapper.Map<Region>(requestDto);
+
+            // Use domain model to update region
+            region = await regionRepository.UpdateAsync(id, region);
+
+            // Check if region is null
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            // Return DTO
+            return Ok(mapper.Map<RegionDto>(region));
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            // Try to delete region
+            var region = await regionRepository.DeleteAsync(id);
+
+            // Check if region is null
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            // Return DTO
+            return Ok(mapper.Map<RegionDto>(region));
+        }
+    }
+}
--- USTrails/USTrails.API/Program.cs
+++ USTrails/USTrails.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using USTrails.API.Data;
+using USTrails.API.Mappings;
+using USTrails.API.Repositories;
 
 namespace USTrails.API
 {
@@ -12,7 +14,6 @@ namespace USTrails.API
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -20,6 +21,10 @@ namespace USTrails.API
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("USTrailsConnectionString"));
             });
+
+            builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+
+            builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
             var app = builder.Build();
 
