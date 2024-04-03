67ea09d965024885a8216ee274400fb0feca8f9a
Matt Burroughs <matt.burro@gmail.com>
2024-04-03 17:05:57 -0400

    Add validation attributes to trail creation and update request models

--- USTrails/USTrails.API/Models/DTO/CreateTrailRequestDto.cs
+++ USTrails/USTrails.API/Models/DTO/CreateTrailRequestDto.cs
@@ -1,12 +1,26 @@
-﻿namespace USTrails.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace USTrails.API.Models.DTO
 {
     public class CreateTrailRequestDto
     {
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(4095)]
         public string Description { get; set; }
+
+        [Required]
         public double LengthInMi { get; set; }
+
         public string? TrailImageUrl { get; set; }
+
+        [Required]
         public Guid DifficultyId { get; set; }
+
+        [Required]
         public List<Guid> StateIds { get; set; }
     }
 }
--- USTrails/USTrails.API/Models/DTO/UpdateTrailRequestDto.cs
+++ USTrails/USTrails.API/Models/DTO/UpdateTrailRequestDto.cs
@@ -1,12 +1,21 @@
-﻿namespace USTrails.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace USTrails.API.Models.DTO
 {
     public class UpdateTrailRequestDto
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double LengthInMi { get; set; }
+        [MaxLength(255)]
+        public string? Name { get; set; }
+
+        [MaxLength(4095)]
+        public string? Description { get; set; }
+
+        public double? LengthInMi { get; set; }
+
         public string? TrailImageUrl { get; set; }
-        public Guid DifficultyId { get; set; }
-        public List<Guid> StateIds { get; set; }
+
+        public Guid? DifficultyId { get; set; }
+
+        public List<Guid>? StateIds { get; set; }
     }
 }
